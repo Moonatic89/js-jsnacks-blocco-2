c828759ed484c971a6ba465456f2091a3496bd5f
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-10-25 12:20:55 +0200

    Finished

---
+++ Asset/js/main.js
@@ -0,0 +1,68 @@
+//Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+//nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+//FAIL
+
+// const element = document.querySelector("h1");
+
+// const showcase = [
+//     {
+//         name: "MontainDew",
+//         weight: 20
+//     }, {
+//         name: "Pininfarina",
+//         weight: 18
+//     }
+//     , {
+//         name: "Asdrubale",
+//         weight: 55
+//     }
+//     , {
+//         name: "Olmo",
+//         weight: 45
+//     }
+// ];
+
+// let arr = [];
+// for (let i = 0; i < showcase.length; i++) {
+
+//     arr.push(showcase[i].weight)
+// }
+
+// const minWeight = Math.min(...arr);
+
+// const bike = showcase.findIndex(minWeight);
+
+
+// element.innerHTML = bike;
+
+
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+// Nome sarà l’unica proprietà da compilare, mentre le altre saranno tutte settate a 0.(modificato)
+
+
+let score = 0;
+let foul = 0;
+const soccerTeam = [
+    {
+        name: "Inter",
+        score,
+        foul
+    }, {
+        name: "Milan",
+        score,
+        foul
+    }, {
+        name: "Genoa",
+        score,
+        foul
+    }, {
+        name: "Sampdoria",
+        score,
+        foul
+    },
+]
+
+
